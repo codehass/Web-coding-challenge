549e74e084449db87af6b5909567d78512a8b820
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-02-08 22:42:13 +0100

    add database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./database');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,7 +9,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+connectDB();
+
+app.get('/todos', (req, res) => {
   res.send('Backend is running...');
 });
 
