7ab7fb110177dac4a7108fa2427a281dca002de7
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-02-10 10:03:14 +0100

    change app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
+import axios from 'axios';
+
+interface Task {
+  id: string;
+  title: string;
+  completed: "pending" | "completed";
+}
 
 function App() {
-  
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/todos");
+      setTasks(response.data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  console.log(tasks);
+  tasks.map(task => console.log(task.title));
+
 
   return (
     <div className="App">
-      hello world
+      <div>
+        <TaskForm fetchTasks={fetchTasks} />
+      </div>
+      <TaskList tasks={tasks} />
+      
     </div>
   )
 }
